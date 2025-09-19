b1c9d20d521c11a9fcfc59e6fbfb5dea44526cb1
Phucdang-gif <danghoangphuc2006@gmail.com>
2025-09-19 22:27:49 +0700

    Update HoaDon.java

--- HoaDon.java
+++ HoaDon.java
@@ -10,13 +10,15 @@ package test;
  */
 import java.io.*;
 import java.util.Arrays;
+import java.util.ArrayList;
 class HoaDon{
     private String maHD;
     private String ngayLap;
     private KhachHang khachHang;
     private NhanVien nhanVienLap;
     private double tongTien;
-    private ChiTietHoaDon[] danhsachCTHD;
+    private ThanhToan hinhThuc;
+    private ArrayList<ChiTietHoaDon> danhsachCTHD;
     
     
     public HoaDon(String maHD,String ngayLap, KhachHang khachHang, NhanVien nhanVienLap){
@@ -24,7 +26,7 @@ class HoaDon{
         this.ngayLap = ngayLap;
         this.khachHang = khachHang;
         this.nhanVienLap = nhanVienLap;
-        this.danhsachCTHD = new ChiTietHoaDon[0];
+        this.danhsachCTHD = new ArrayList<>();
         this.tongTien = 0;
     }
     
@@ -40,55 +42,59 @@ class HoaDon{
     public void setNgayLap(String ngayLap){
         this.ngayLap = ngayLap;
     }
+    
     public KhachHang getKhachHang(){
         return khachHang;
     }
+    
     public void setKhachHang(KhachHang khachHang){
         this.khachHang = khachHang;
     }
+    
     public NhanVien getNhanVienLap(){
         return nhanVienLap;
     }
+    
     public void setNhanVienLap(NhanVien nhanVienLap){
         this.nhanVienLap = nhanVienLap;
     }
     
-    
     public void themSP (SanPham sp, int soLuongMua){
         ChiTietHoaDon cthd = new ChiTietHoaDon(sp,soLuongMua);
-        danhsachCTHD = Arrays.copyOf(danhsachCTHD, danhsachCTHD.length+1);
-        danhsachCTHD[danhsachCTHD.length - 1] = cthd;
+        danhsachCTHD.add(cthd);
     }
+    
     public void xoaSP(String maSP){
-        for (int i=0;i<danhsachCTHD.length;i++){
-            if (danhsachCTHD.getSanPham().getmaSP().equals(maSP)){
-                for (int j=i;j<danhsachCTHD.length-1;j++){
-                    danhsachCTHD[j] = danhsachCTHD[j+1];
-                }
-                danhsachCTHD = Arrays.copyOf(danhsachCTHD, danhsachCTHD.length-1);
+        for (int i=0;i<danhsachCTHD.size();i++){
+            if (danhsachCTHD.get(i).getSanPham().equals(maSP)){
+                danhsachCTHD.remove(i);
                 break;
             }
         }
     }
+    
     public double tinhTongTien(){
         tongTien = 0;
-        for (int i=0;i<danhsachCTHD.length;i++){
-            tongTien+=danhsachCTHD[i].getThanhTien();
+        for (int i=0;i<danhsachCTHD.size();i++){
+            ChiTietHoaDon cthd = danhsachCTHD.get(i);
+            tongTien += cthd.getThanhTien();
         }
         return tongTien;
     }
+    
     public void xuatHoaDon() {
        System.out.println("=== HÓA ĐƠN " + maHD + " ===");
        System.out.println("Ngày lập: " + ngayLap);
        System.out.println("Khách hàng: " + khachHang.getTenKH());
        System.out.println("Nhân viên lập: " + nhanVienLap.getTenNV());
        System.out.println("Danh sách sản phẩm:");
-       for (int i = 0; i < danhSachCTHD.length; i++) {
-           SanPham sp = danhSachCTHD[i].getSanPham();
+       for (int i = 0; i < danhsachCTHD.size(); i++) {
+           ChiTietHoaDon cthd = danhsachCTHD.get(i);
+           SanPham sp = cthd.getSanPham();
            System.out.println(" - " + sp.getTenSP() 
-                             + " | SL: " + danhSachCTHD[i].getSoLuongMua()                               
+                             + " | SL: " + cthd.getSoLuongMua()                               
                              + " | Giá: " + sp.getGia()
-                             + " | Thành tiền: " + danhSachCTHD[i].getThanhTien());
+                             + " | Thành tiền: " + cthd.getThanhTien());
         }
         System.out.println("Tổng tiền: " + tinhTongTien());
     }
