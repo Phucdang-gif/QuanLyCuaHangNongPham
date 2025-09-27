81a4dc4895962c1d4f5065f0e9344c6d8e5c81e9
lythanh-277 <lydao.030406@gmail.com>
2025-09-27 19:27:08 +0700

    Update NhanVien.java lần 2

--- NhanVien.java
+++ NhanVien.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 
 public class NhanVien {
-    private int maNhanVien;
-    private string tenNhanVien;
-    private string hoNhanVien;
-    private int luongNhanVien;
-    
-    public NhanVien(int mnv,string tnv,string hnv,int luongnv){
+    private String maNhanVien;
+    private String tenNhanVien;
+    private String hoNhanVien;
+    private double luongNhanVien;
+    private String gioiTinh;
+    public NhanVien(){
+        maNhanVien="";
+        tenNhanVien="";
+        hoNhanVien="";
+        luongNhanVien=0;
+    };
+    public NhanVien(String mnv,String tnv,String hnv,String gt,double luongnv){
         maNhanVien=mnv;
         tenNhanVien=tnv;
         hoNhanVien=hnv;
+        gioiTinh=gt;
         luongNhanVien=luongnv;
 
     }
@@ -17,34 +24,51 @@ public class NhanVien {
         maNhanVien=nv1.maNhanVien;
         tenNhanVien=nv1.tenNhanVien;
         hoNhanVien=nv1.hoNhanVien;
+        gioiTinh=nv1.gioiTinh;
         luongNhanVien=nv1.luongNhanVien;
+
     }
-     public int getMNV(){return maNhanVien;}
-    public string getTenNhanVien(){return tenNhanVien;}
-    public string getHoNhanVien(){return hoNhanVien;}
-    public int getLNV(){return luongNhanVien;}
-    public void setLNV(int luongNhanVien){
+    public String getMNV(){return maNhanVien;}
+    public String getTenNhanVien(){return tenNhanVien;}
+    public String getHoNhanVien(){return hoNhanVien;}
+    public double getLNV(){return luongNhanVien;}
+    public String getGT(){return gioiTinh;}
+    public void setLNV(double luongNhanVien){
         this.luongNhanVien=luongNhanVien;
     }
-    public void setNV(int maNhanVien,string tenNhanVien,string hoNhanVien){
+    public void setNV(String maNhanVien,String tenNhanVien,String hoNhanVien,String gioiTinh){
         this.maNhanVien=maNhanVien;
        this.tenNhanVien=tenNhanVien; 
         this.hoNhanVien=hoNhanVien;
+        this.gioiTinh=gioiTinh;
     }
-    public nhapNhanVien(){
+    public void setMNV(String mnv){
+            this.maNhanVien=mnv;
+    }
+    public void setTNV(String tnv){
+            this.tenNhanVien=tnv;
+    }
+    public void setHNV(String hnv){
+            this.hoNhanVien=hnv;
+    }
+    public void setGT(String gt){
+            this.gioiTinh=gt;
+    }
+    public void nhapNhanVien(){
         Scanner sc=new Scanner(System.in);
-        maNhanVien=sc.nextInt();
+        maNhanVien=sc.nextLine();
         tenNhanVien=sc.nextLine();
         hoNhanVien=sc.nextLine();
-        luongNhanVien=sc.nextInt();
+        gioiTinh=sc.nextLine();
+        luongNhanVien=sc.nextDouble();
 
     }
-    public xuatNhanVien(){
-        System.out.println(maNhanVien);
-        System.out.println(tenNhanVien);
-        System.out.println(hoNhanVien);
-        System.out.println(luongNhanVien);
+    public void xuatNhanVien(){
+        System.out.println(" ma nhan vien la: "+maNhanVien);
+        System.out.println("ten nhan vien: "+tenNhanVien);
+        System.out.println("ho nhan vien: "+hoNhanVien);
+        System.out.println("gioi tinh cua nhan vien la: "+gioiTinh);
+        System.out.println("luong nhan vien: "+luongNhanVien);
     }
 
 }
-
